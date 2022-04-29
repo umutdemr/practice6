0f10b28a05d5b2a35efd11541b80d954501f4b93
Umut Can Demir <umutcdemir@gmail.com>
2022-04-29 09:54:53 +0300

    .

---
+++ src/app/compo/compo.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CompoComponent } from './compo.component';
+
+describe('CompoComponent', () => {
+  let component: CompoComponent;
+  let fixture: ComponentFixture<CompoComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CompoComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CompoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
